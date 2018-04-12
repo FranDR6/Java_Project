f33a14d0a17ec0b0cea06090f202bd5803fefc30
Vladek <frandr96@gmail.com>
2018-04-12 10:42:28 +0200

    q

---
+++ Proyectos/Sudoku/Tablero.java
@@ -0,0 +1,101 @@
+package Sudoku;
+
+public class Tablero {
+	private Casilla[][] sudoku = new Casilla[9][9];
+
+	public Casilla[][] getSudoku() {
+		return sudoku;
+	}
+
+	public void setSudoku(Casilla[][] sudoku) {
+		this.sudoku = sudoku;
+	}
+
+	public Tablero(Casilla[][] sudoku) {
+		super();
+		for (int x = 0; x < this.sudoku.length; x++) {
+			for (int y = 0; y < this.sudoku.length; y++) {
+				this.sudoku[x][y] = new Casilla();
+				asignarArea(x, y, this.sudoku);
+			}
+		}
+		do {
+			asiganarNumeros(this.sudoku);
+		} while (validarSudoku(this.sudoku) == true);
+
+		this.sudoku = sudoku;
+	}
+
+	private static boolean validarSudoku(Casilla[][] sudoku) {
+		for (int a = 0; a < sudoku.length; a++) {
+			for (int b = 0; b < sudoku.length; b++) {
+				for (int e = 0; e < sudoku.length; e++) {
+					if (sudoku[a][b].getNumero() == sudoku[a][e].getNumero() && e != b) {
+						return true;
+					}
+					if (sudoku[a][b].getNumero() == sudoku[e][b].getNumero() && e != a) {
+						return true;
+					}
+				}
+				for (int i = 0; i < sudoku.length; i++) {
+					for (int j = 0; j < sudoku.length; j++) {
+						if (sudoku[a][b].getNumero() == sudoku[i][j].getNumero()
+								&& sudoku[a][b].getArea() == sudoku[i][j].getArea() && i != a && j != b) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private void asiganarNumeros(Casilla[][] sudoku) {
+		for (int a = 0; a < this.sudoku.length; a++) {
+			for (int b = 0; b < this.sudoku.length; b++) {
+				int intentos = 300;
+				do {
+					this.sudoku[a][b].setNumero((int) (Math.random() * 8) + 1);
+					intentos--;
+				} while (validarCasilla(this.sudoku, a, b) == true || intentos > 0);
+			}
+		}
+	}
+
+	private boolean validarCasilla(Casilla[][] sudoku, int a, int b) {
+		for (int x = 0; x < sudoku.length; x++) {
+			if (this.sudoku[a][b].getNumero() == this.sudoku[a][x].getNumero() && x != b) {
+				return true;
+			}
+			if (this.sudoku[a][b].getNumero() == this.sudoku[x][b].getNumero() && x != a) {
+				return true;
+			}
+		}
+		for (int i = 0; i < sudoku.length; i++) {
+			for (int j = 0; j < sudoku.length; j++) {
+				if (this.sudoku[a][b].getNumero() == this.sudoku[i][j].getNumero()
+						&& this.sudoku[a][b].getArea() == this.sudoku[i][j].getArea() && i != a && j != b) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static void asignarArea(int x, int y, Casilla[][] sudoku) {
+		int a = 0;
+		int[][] filas = { { 0, 0, 0, 1, 1, 1, 2, 2, 2 }, { 3, 3, 3, 4, 4, 4, 5, 5, 5 }, { 6, 6, 6, 7, 7, 7, 8, 8, 8 } };
+		if (x <= 2) {
+			a = 0;
+			sudoku[x][y].setArea(filas[a][y]);
+		}
+		if (x >= 3 && x <= 5) {
+			a = 1;
+			sudoku[x][y].setArea(filas[a][y]);
+		}
+		if (x >= 6) {
+			a = 2;
+			sudoku[x][y].setArea(filas[a][y]);
+		}
+	}
+}
